d413bc554665079f20b855a6f53ebf7cd679b0a0
Joe Nosie <182554+BoltsJ@users.noreply.github.com>
2021-06-22 20:51:23 -0500

    Export LancerCombatant

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { LancerCombat, Activations, isActivations } from "./module/lancer-combat";
+export { LancerCombat, LancerCombatant, Activations, isActivations } from "./module/lancer-combat";
 export { LancerCombatTracker } from "./module/lancer-combat-tracker";
 export { LIForm } from "./module/li-form";
